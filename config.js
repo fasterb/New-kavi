6cc6693402ca0a24c8c1b754de38bb33a007c964
fasterb <pmyownwabot@gmail.com>
2024-11-08 16:55:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID "jFAzjBjB#Ha_cXB0fgTngYBHNuOf6feO5ewS1haVcOx9hIbmF8GY",
+SESSION_ID: process.env.SESSION_ID " WEx1nApb#Zv5n2pMUlYKYqkCB0F7aKoVCoC8njzhdjF10hvuRyso ",
 ALIVE_IMG: process.env.ALIVE_IMG || "htttp://avatars.githubusercontent.com/u/106251140?v=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello I am KAVI-MD i am alive now!",
 };
